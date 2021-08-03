b1a06aae35cae4ee718782fc4e1c802459466692
victorguirra <victor.hugo.guirra@gmail.com>
2021-08-02 21:02:36 -0300

    update size in modal

--- src/components/Modal/styles.js
+++ src/components/Modal/styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Overlay = styled.div`
     width: 100%;
-	height: 100%;
+	height: 100vh;
 
 	background-color: rgba(0, 0, 0, 0.6);
 	backdrop-filter: blur(5px);
