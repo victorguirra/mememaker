4e75ebab0f5be6ce26145cf18f667f660f60640f
victorguirra <victor.hugo.guirra@gmail.com>
2021-08-02 21:38:00 -0300

    remove modal and toast components

--- src/pages/Result/index.js
+++ src/pages/Result/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import FileSaver from 'file-saver'
 import { Container, NewMemeButton } from './styles';
 
@@ -6,41 +6,13 @@ import { useMeme } from '../../contexts/memeContext';
 
 import Card from '../../components/Card';
 import Button from '../../components/Button';
-import Modal from '../../components/Modal';
-import Toast from '../../components/Toast';
 
 export default function Result() {
-    const [ modalIsOpen, setModalIsOpen ] = useState(false);
-    const [ fileName, setFileName ] = useState('');
-    const [ toastData, setToastData ] = useState({
-        isOpen: false,
-        message: ''
-    })
-
     const { generatedMeme } = useMeme();
 
-    function handleDownload(event) {
-        event.preventDefault();
-
-        if(!fileName) {
-            setToastData({
-                isOpen: true,
-                message: 'O Nome do arquivo é obrigatório!' 
-            })
-            return;
-        }
-
-        FileSaver.saveAs(generatedMeme, fileName);
-        setModalIsOpen(false);
+    function handleDownload() {
+        FileSaver.saveAs(generatedMeme, 'Meme');
     }
-
-    useEffect(() => {
-        if(toastData.isOpen){
-            setTimeout(() => {
-                setToastData({ isOpen: false, message: ''});
-            }, 3000);
-        }
-    }, [toastData])
 
     return(
         <Container>
@@ -48,24 +20,8 @@ export default function Result() {
                 <img src={ generatedMeme } alt="Generated Meme" />
 
                 <NewMemeButton to="/">Gerar outro meme</NewMemeButton>
-                <Button onClick={ () => setModalIsOpen(true) }>Fazer Download</Button>
 
-                { modalIsOpen && (
-                    <Modal>
-                        { toastData.isOpen && <Toast message={ toastData.message } /> }
-
-                        <h2>Digite o nome do arquivo</h2>
-                        
-                        <form onSubmit={ handleDownload }>
-                            <input
-                                type="text"
-                                onChange={ event => setFileName(event.target.value)}
-                                placeholder="Nome do arquivo"
-                            />
-                            <Button type="submit">Fazer download</Button>
-                        </form>
-                    </Modal>
-                )}
+                <Button onClick={ handleDownload }>Fazer Download</Button>
             </Card>
         </Container>
     )
